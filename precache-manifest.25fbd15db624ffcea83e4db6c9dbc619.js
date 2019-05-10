0ff783ef2143291a8a6341ad128c60b9007aeb75
hopdt <hop.dt@neo-lab.vn>
2019-05-10 10:34:49 +0700

    Updates

---
+++ precache-manifest.25fbd15db624ffcea83e4db6c9dbc619.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a172c8fbfa148addccf",
+    "url": "/discuss/static/js/runtime~main.3a172c8f.js"
+  },
+  {
+    "revision": "556b29828747564748c9",
+    "url": "/discuss/static/js/main.556b2982.chunk.js"
+  },
+  {
+    "revision": "875430c4bcd697de821d",
+    "url": "/discuss/static/js/2.875430c4.chunk.js"
+  },
+  {
+    "revision": "556b29828747564748c9",
+    "url": "/discuss/static/css/main.942e9839.chunk.css"
+  },
+  {
+    "revision": "01b2d14da051c4e29cc68c1154450d31",
+    "url": "/discuss/index.html"
+  }
+];
