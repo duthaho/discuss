3dba401351217faf221a88a1e8c38860a76e819c
hopdt <hop.dt@neo-lab.vn>
2019-05-15 10:32:39 +0700

    deploy server to heroku

--- client/src/util/api.js
+++ client/src/util/api.js
@@ -1,7 +1,7 @@
 const baseUrl =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:8080/api'
-    : `https://${window.location.hostname}/api`;
+    : 'https://htdiscuss.herokuapp.com/api';
 
 const methods = {
   get: async function (endpoint, token = null) {
